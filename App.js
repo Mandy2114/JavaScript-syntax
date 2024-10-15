34645d435bbf2481e8c3f9f9a563864471524014
Mandy2114 <mandydecamp1213@gmail.com>
2024-10-15 10:59:14 -0400

    Finish Runing code

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ const nums = [13, 87, 2, 89, 12, 4, 90, 63];
 const doubled = nums.map(num => num * 2);
 
 console.log(doubled)
+console.log("Exercise 1");
 ///// Exercise 2 ////////
 
 const pizzaToppings = ['Pineapple', 'Olives', 'Anchovies'];
@@ -13,6 +14,7 @@ const [firstTopping, secondTopping] = pizzaToppings;
 
 console.log(firstTopping);
 console.log(secondTopping);
+console.log("Exercise 2");
 ///// Exercise 3 ////////
 
 const car = {
@@ -24,14 +26,24 @@ const { make, model } = car;
 
 console.log(make);
 console.log(model);
+console.log("Exercise 3");
 ///// Exercise 4 ////////
 
 const controversialPizzaToppings = [...pizzaToppings];
 
 console.log(controversialPizzaToppings);
+console.log("Exercise 4");
 ///// Exercise 5 ////////
+// const car = {
+//   make: 'Audi',
+//   model: 'q5',
+// };
 
 const myCar = { ...car, model: 'q7' };
+
+console.log(car);
+console.log(myCar);
+console.log("Exercise 5");
 ///// Exercise 6 ////////
 
 const userProfile = {};
@@ -40,8 +52,9 @@ const propertyName = 'username';
 userProfile[propertyName] = 'Jane.Doe';
 
 console.log(userProfile);
+console.log("Exercise 6");
 ///// Exercise 7 ////////
-
+console.log("Exercise 7");
 ///// Exercise 8 ////////
 
 function describeAnimal(noun = 'cat', adjective = 'white') {
@@ -49,6 +62,7 @@ function describeAnimal(noun = 'cat', adjective = 'white') {
 }
 
 describeAnimal();
+console.log("Exercise 8");
 ///// Exercise 9 ////////
 
 let pizza = 'tasty';
@@ -60,6 +74,7 @@ if (pizza === 'tasty') {
 }
 
 pizza === 'tasty' ? console.log('yum') : console.log('yuck');
+console.log("Exercise 9");
 ///// Exercise 10 ////////
 
 const localLangConfig = null;
@@ -69,9 +84,9 @@ console.log('Language setting:', LANG);
 const userSavedTheme = null;
 const USER_THEME = userSavedTheme || 'light'
 console.log('User theme setting:', USER_THEME);
+console.log("Exercise 10");
 
 ///// Exercise 11 ////////
-// Now check for `cat.age` on `adventurer`. See how it errors out? Use optional chaining in a console.log that it returns undefined instead.
 
 const adventurer = {
   name: 'Alice',
@@ -81,3 +96,4 @@ const adventurer = {
 let cat = adventurer.cat?.name; 
 
 console.log(cat);
+console.log("Exercise 11");
